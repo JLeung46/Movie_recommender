e9f2bfc5bd690e27f3199d30b448914a333bd769
Jack Leung <jleungk46@gmail.com>
2017-02-07 16:57:03 -0500

    added code for evaluation

--- item_based_recc.py
+++ item_based_recc.py
@@ -3,75 +3,107 @@ import pandas as pd
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
 from time import time
-
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
 class ItemItemRecommender(object):
 
     def __init__(self, neighborhood_size):
         self.neighborhood_size = neighborhood_size
 
-    def fit(self, ratings_mat):
-        self.ratings_mat = ratings_mat
-        self.n_users = ratings_mat.shape[0]
-        self.n_items = ratings_mat.shape[1]
-        self.item_sim_mat = cosine_similarity(self.ratings_mat.T)
+    def fit(self, ratings_sparse):
+        self.ratings_sparse = ratings_sparse
+        self.n_users = ratings_sparse.shape[0]
+        self.n_items = ratings_sparse.shape[1]
+        self.item_sim_mat = cosine_similarity(self.ratings_sparse.T)
         self._set_neighborhoods()
 
     def _set_neighborhoods(self):
         least_to_most_sim_indexes = np.argsort(self.item_sim_mat, 1)
         self.neighborhoods = least_to_most_sim_indexes[:, -self.neighborhood_size:]
 
-    def pred_one_user(self, user_id, report_run_time=False):
+    def pred_one_user(self, user_id, show_run_time=False):
         start_time = time()
-        items_rated_by_this_user = self.ratings_mat[user_id].nonzero()[1]
-        # Just initializing so we have somewhere to put rating preds
+        items_rated_by_this_user = self.ratings_sparse[user_id].nonzero()[1]
+        # Define somewhere to put rating predictions
         out = np.zeros(self.n_items)
         for item_to_rate in range(self.n_items):
             relevant_items = np.intersect1d(self.neighborhoods[item_to_rate],
                                             items_rated_by_this_user,
-                                            assume_unique=True)  # assume_unique speeds up intersection op
-            out[item_to_rate] = self.ratings_mat[user_id, relevant_items] * \
+                                            assume_unique=True)
+            out[item_to_rate] = self.ratings_sparse[user_id, relevant_items] * \
                 self.item_sim_mat[item_to_rate, relevant_items] / \
                 self.item_sim_mat[item_to_rate, relevant_items].sum()
-        if report_run_time:
+        if show_run_time:
             print("Execution time: %f seconds" % (time()-start_time))
-        cleaned_out = np.nan_to_num(out)
+        cleaned_out = np.nan_to_num(out) # Converts nans to 0
         return cleaned_out
 
-    def pred_all_users(self, report_run_time=False):
+    def pred_all_users(self, show_run_time=False):
         start_time = time()
         all_ratings = [
             self.pred_one_user(user_id) for user_id in range(self.n_users)]
-        if report_run_time:
+        if show_run_time:
             print("Execution time: %f seconds" % (time()-start_time))
         return np.array(all_ratings)
 
     def top_n_recs(self, user_id, n):
+        '''
+        Predicts top n movie ratings for a single user.
+
+        Input: user id
+        Output: 
+        '''
         pred_ratings = self.pred_one_user(user_id)
         item_index_sorted_by_pred_rating = list(np.argsort(pred_ratings))
-        items_rated_by_this_user = self.ratings_mat[user_id].nonzero()[1]
+        items_rated_by_this_user = self.ratings_sparse[user_id].nonzero()[1]
         unrated_items_by_pred_rating = [item for item in item_index_sorted_by_pred_rating
                                         if item not in items_rated_by_this_user]
         return unrated_items_by_pred_rating[-n:]
 
+    def predict(self,test,all_preds):
+        preds = []
+        for user_id in xrange(self.n_users):
+            movies = test[test['user'] == user_id+1]['movie']
+            for i,rating in enumerate(all_preds[user_id]):
+                if i+1 in movies.values:
+                    preds.append(rating)
+        clean_preds = np.nan_to_num(preds) # convert nans to 0
+        result = sqrt(mean_squared_error(test['rating'], clean_preds))
+        return result
+
 
 def get_ratings_data():
-    ratings_df = pd.read_table("data/ratings",
-                                     names=["user", "movie", "rating", "timestamp"])
-    highest_user_id = ratings_df.user.max()
-    highest_movie_id = ratings_df.movie.max()
-    ratings_as_mat = sparse.lil_matrix((highest_user_id, highest_movie_id))
-    for _, row in ratings_df.iterrows():
-            # subtract 1 from id's due to match 0 indexing
-        ratings_as_mat[row.user-1, row.movie-1] = row.rating
-    return ratings_df, ratings_as_mat
+    '''
+    Reads in train and test data and creates a sparse matrix using train data.
+
+    Output: Train DataFrame, Test DataFrame, Sparse Train Matrix. 
+    '''
+    train = pd.read_table("data/ua.base", names=["user", "movie", "rating", "timestamp"])
+    test = pd.read_table("data/ua.test", names =["user", "movie", "rating", "timestamp"])
+    highest_user_id = train.user.max()
+    highest_movie_id = train.movie.max()
+    ratings_sparse_mat = sparse.lil_matrix((highest_user_id, highest_movie_id))
+    for _, row in train.iterrows():
+        ratings_sparse_mat[row.user-1, row.movie-1] = row.rating  # subtract 1 from id's to match 0 indexing
+    return train, test, ratings_sparse_mat
+
+
+
 
 
 if __name__ == "__main__":
-    ratings_data_contents, ratings_mat = get_ratings_data()
+    train_data, test_data, ratings_sparse = get_ratings_data()
     my_rec_engine = ItemItemRecommender(neighborhood_size=75)
-    my_rec_engine.fit(ratings_mat)
+    my_rec_engine.fit(ratings_sparse)
+    all_predictions = my_rec_engine.pred_all_users(report_run_time=True)
+    score = my_rec_engine.predict(test_data,all_predictions)
+    print score
+
+    '''
     user_1_preds = my_rec_engine.pred_one_user(user_id=1, report_run_time=True)
+
     # Show predicted ratings for user #1 on first 100 items
     print(user_1_preds[:100])
     print(my_rec_engine.top_n_recs(2, 20))
+    '''
